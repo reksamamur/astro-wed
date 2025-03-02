d8eb2ac846b7e56536c8a131aab9e3a08c773c96
reksamamur <reksa.kuana9@gmail.com>
2025-03-02 13:00:52 +0700

    first: commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import nodejs from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  adapter: nodejs({
+    mode: "standalone",
+  }),
+  output: "server",
+});
